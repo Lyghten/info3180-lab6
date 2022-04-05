af1419c078cee109a70aa1374fee3a0e6b634f1c
Raphiel Collins <Lyghten>
2022-04-04 22:20:58 -0500

    Commit

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,15 +1,41 @@
 <template>
-    <ul class="news__list">
-        <li class="news__item">News item 1</li>
-        <li class="news__item">News item 2</li>
-        <li class="news__item">News item 3</li>
-    </ul>
+	<ul class="news__list">
+			<li v-for="article in articles" class="news__item">{{article.title}} 
+			<img class="images" :src = "article.urlToImage"/>
+			{{article.description}}</li>
+			
+			<form @submit.prevent="searchNews" class="d-flex flexcolumn justify-content-center">
+                </form>
+		                                                   
+	</ul>
 </template>
+
 
 <script>
 export default {
-    data() {
-        return {};
-    },
-};
+	data() {
+	  return {
+	      articles: []
+	      searchTerm:''
+	  }
+     },
+     created() {
+     	   let self= this;
+           fetch('https://newsapi.org/v2/top-headlines?country=us',
+         {
+      headers: {
+          'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+      }
+  })
+           .then(function(response){
+             return response.json();
+           })
+           .then(function(data) {
+             console.log(data);
+             self.articles= data.articles;
+           });
+        }           
+           
+     };
+     
 </script>
